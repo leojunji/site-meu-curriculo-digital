bf7c4ca890a18788ae0218f87e639571b388eab0
leojunji <leojunji@outlook.com>
2024-03-16 13:43:56 -0300

    Create experience.js

---
+++ js/experience.js
@@ -0,0 +1,66 @@
+let experiencesData = [
+    {
+      role: 'Estágio em TI ', //0
+      company: 'Pumatronix', 
+      activities: 'Tinha a responsabilidade de assegurar o funcionamento dos ' +
+       'servidores da empresa. Além disso, era responsável pela documentação dos projetos de TI ' +
+      'da empresa e pela manutenção e inventário dos equipamentos de TI utilizados pelos' +
+      'funcionários.',
+    },
+    {
+      role: 'Suporte de TI', //1
+      company: 'IMAP', 
+      activities: 'Tinha a responsabilidade de assegurar o funcionamento dos ' +
+       'servidores da empresa. Além disso, era responsável pela documentação dos projetos de TI ' +
+      'da empresa e pela manutenção e inventário dos equipamentos de TI utilizados pelos' +
+      'funcionários.',
+    }
+  ];
+  
+
+
+//cria a section curriculum__text__experience
+function createExperienceSection(experience) {
+
+
+  let curriculumTextExperience = document.createElement('section'); //criando <section>
+  curriculumTextExperience.className = 'curriculum__text__unit experience';
+
+  let tittleRole = document.createElement('h2'); //criando <h2>
+  tittleRole.textContent = experience.role;
+
+  let paragraphCompany = document.createElement('p'); //criando <p> para company
+  paragraphCompany.className = 'company';
+  paragraphCompany.textContent = experience.company;
+
+  let paragraphActivities = document.createElement('p'); //criando <p> para activities
+  paragraphActivities.className = 'details';
+  paragraphActivities.textContent = experience.activities;
+
+  curriculumTextExperience.appendChild(tittleRole);
+  curriculumTextExperience.appendChild(paragraphCompany);
+  curriculumTextExperience.appendChild(paragraphActivities);
+  
+
+  return curriculumTextExperience;
+
+};
+
+//coloca a section dentro do curriculum__text__experience
+function displayExperience(experience, parentElement) {
+  
+  parentElement.appendChild(createExperienceSection(experience));
+};
+
+function showAllExperiences() {
+  for (let i  = 0; i < experiencesData.length; i++) {
+    displayExperience(experiencesData[i],curriculumText)
+  }
+}
+
+let experienceButton = document.querySelector('.experience_button');
+
+experienceButton.onclick = function() {
+  curriculumText.innerHTML = "";
+  showAllExperiences();
+}
