a2845e44c0f91bde7be9f4fcbc0044661db60c5d
leojunji <leojunji@outlook.com>
2024-04-25 20:39:26 -0300

    Update main.ts
    this file were reset. further ahead will be added imports when i update my js code to ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,1 @@
-function redirectToAboutPage() {
-  // Redirecionar para index.html
-  window.location.href = "index.html";
-}
 
-// Chame a função para redirecionar assim que o arquivo main.ts for carregado
-redirectToAboutPage();
