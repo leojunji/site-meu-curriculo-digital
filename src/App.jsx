5808557e1e9e9563c3e2777ae4bc1e07d1c9aed8
leojunji <leojunji@outlook.com>
2024-06-13 01:50:39 -0300

    LoadData in App
    The data will be pull from "server", when i open the site, so i wont have to wait at "sobre mim" page

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,30 @@ import NoPage from "./pages/NoPage";
 import "./Reset.css";
 import "./App.css";
 import "./globalClass.css";
-
+import { useState, useMemo } from "react";
+import LoadData from "./components/LoadData";
 const App = () => {
+  const [dataPageAbout] = useState([
+    {
+      id: "1dd58d933e9a4977a87451d9a5684038",
+      name: "skillsData",
+    },
+    {
+      id: "d050de821f776e269c38c2f5223b1bc8",
+      name: "experiencesData",
+    },
+    {
+      id: "0acce4c14afbab07e0420f9f04e16d29",
+      name: "idiomsData",
+    },
+  ]);
+
+  useMemo(() => {
+    dataPageAbout.map((item) => {
+      LoadData(item);
+    });
+  }, [dataPageAbout]);
+
   return (
     <div className="App">
       <BrowserRouter>
