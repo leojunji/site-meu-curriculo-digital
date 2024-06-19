f545f38e259221f6857eb78d4d3540f0568721ce
leojunji <leojunji@outlook.com>
2024-06-19 18:51:10 -0300

    Update index.js
    errors now is using prefix, and, is more accurate
    
    - msgInfo
    -- msgError
    > msgSystem

--- src/components/StoreInSessionData/index.js
+++ src/components/StoreInSessionData/index.js
@@ -1,19 +1,20 @@
 import getDataLatestGist from "../GistContent";
+import { ERROR_PREFIX, INFO_PREFIX } from "../../constants";
 
 const StoreInSessionData = async (item) => {
   try {
-    if (sessionStorage.getItem(item.name)) {
-      console.log(`INFO:: data for page was already loaded in session`);
-    } else {
-      console.log("INFO:: loading data from server");
+    // if (sessionStorage.getItem(item.name)) {
+    //   console.log(`INFO:: data for page was already loaded in session`);
+    // } else {
 
-      let data = await getDataLatestGist(item.id);
-      if (data.length > 0) {
-        sessionStorage.setItem(item.name, JSON.stringify(data));
-      }
+    let data = await getDataLatestGist(item.id);
+    if (data.length > 0) {
+      sessionStorage.setItem(item.name, JSON.stringify(data));
+      console.log(`${INFO_PREFIX} -DATA STORED IN SESSION`);
     }
+    // }
   } catch (error) {
-    console.log("ERROR:: : " + error);
+    console.log(`${ERROR_PREFIX} >" + ${error}`);
     sessionStorage.clear();
   }
 };
